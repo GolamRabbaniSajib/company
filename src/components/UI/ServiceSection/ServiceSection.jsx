60b0e5625bb12473dda26b0bcc6359a01cef053f
sbsajibsx <golamrobbanisajib@gmail.com>
2025-05-21 00:51:44 +0600

    modify servicedata

--- src/components/UI/ServiceSection/ServiceSection.jsx
+++ src/components/UI/ServiceSection/ServiceSection.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { motion, AnimatePresence } from "framer-motion";
-import services from "@/data/servicesdata";
+import services from "../../../Data/servicesdata";
 
 const ServicesSection = () => {
   const [active, setActive] = useState(0);
